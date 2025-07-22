73b1c91b43c96606a4cb823086426799b79c5046
TanqDev88 <oliverprim783@gmail.com>
2025-07-21 23:36:46 -0300

    feat:update onb mocks

--- mock-server/mocks/onb.js
+++ mock-server/mocks/onb.js
@@ -1,44 +1,76 @@
-// mocks/ONB.js
+// ========================================================================================
+//             CASOS DE ERROR/EXITO SERVICIOS ALTA CLIENTE & CUENTA INDIVIDUO
+// ========================================================================================
+
+// Simulaciones de respuestas de los servicios de apertura de cuenta (ONB). 
+// (casos de error y de éxito para los servicios `altaCliente` y `cuentaIndividuo`).
 
 module.exports = {
     altaCliente: {
+        // Error de autenticación (token inválido o expirado)
         '401': [401, { error_code: 'Error de autenticación', message: 'mensaje' }],
+        
+        //Cliente (caso duplicado)
         '412': [412, { error_code: '101061', message: 'duplicado' }],
+        
+        // Errores genéricos en la creación del cliente
         '103001': [412, { error_code: '103001', message: 'error en la creación del cliente' }],
         '103005': [412, { error_code: '103005', message: 'error en la creación del cliente' }],
         '101001': [412, { error_code: '101001', message: 'error en la creación del cliente' }],
+        
+        // DNI inhabilitado.
         '107215': [412, { error_code: '107215', message: 'inhabilitación de DNI' }],
+        
+        // Dirección de email repetido.
         '107526': [412, { error_code: '107526', message: 'mail repetido' }],
+        
+        // Error en sucursal
         '701102': [412, { error_code: '701102', message: 'error en sucursal' }],
+        
+        // Error inesperado del servidor
         '500': [500, { error_code: 'service exception', message: 'error inesperado' }],
     },
+
     cuentaIndividuo: {
+        // Error de autenticación (token inválido o expirado)
         '401': [401, {
             error_code: 'SecurityException',
             message: 'La información de seguridad no es válida',
         }],
+        
+        // Límite máximo de cuentas alcanzado para el cliente
         '258264': [412, {
             error_code: '258264',
             message: 'SE SUPERO LA CANTIDAD MAXIMA DE CUENTAS PARA EL CLIENTE',
         }],
+        
+        // Ente no existente o cedula incorrecta.
         '101042': [412, {
             error_code: '101042',
             message: 'NO EXISTE ENTE O CEDULA INCORRECTA',
         }],
+        
+        // Oficial de cuenta no disponible.
         '258235': [412, {
             error_code: '258235',
             message: 'EL OFICIAL DE CUENTA NO SE ENCUENTRA PARA LA SUCURSAL O NO ESTA VIGENTE',
         }],
+        
+        // Apertura de cuenta denegada (ir a sucursal).
         '258266': [412, {
             error_code: '258266',
             message: 'No pudimos abrir tu Caja de Ahorro por este canal. Por favor, acercate a una sucursal',
         }],
+        
+        // Error 500
         '500': [500, {
             error_code: 'ServiceException',
             message: 'Error interno, por favor vuelva a intentar.',
         }],
     },
+
     respuestasExito: {
+        // cuenta abierta exitosamente en USD
         cuentaUSD: {
             cuenta_banco: '470309582121183',
             cbu: '2850703540095821211838',
@@ -47,6 +79,8 @@ module.exports = {
             oficial: 232321,
             producto_bancario: 21,
         },
+
+        // Cuenta abierta exitosamente en ARS
         cuentaARS: {
             cuenta_banco: '470309582121183',
             cbu: '2850703540095821211838',
@@ -57,3 +91,4 @@ module.exports = {
         },
     }
 };
+
